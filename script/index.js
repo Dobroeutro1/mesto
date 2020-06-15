b621701ea50d3ff7fb8e34bdaccecfefe086fde5
Artem Noskov <artyom.noskov.1998@yandex.ru>
2020-06-15 21:43:52 +0500

    finish

---
+++ script/index.js
@@ -0,0 +1,34 @@
+// Открытие/закрытие поп-апа
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-btn');
+let closeButton = document.querySelector('.popup__close-btn');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#about');
+let profileName = document.querySelector('.profile__title');
+let profileAbout = document.querySelector('.profile__subtitle');
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
